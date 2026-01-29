d6919ac91daf64d0bb9d957134e3ab217136c543
장재형[AI Product] <jaehyung.jang@navercorp.com>
2026-01-30 01:43:53 +0900

    Switch to adapter-auto for Vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,13 @@
-import adapter from '@sveltejs/adapter-static';
+// static을 auto로 바꿉니다!
+import adapter from '@sveltejs/adapter-auto'; 
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-//		paths: {
-//			base: process.env.NODE_ENV === 'production' ? '/my-roulette' : '',
-//		}
+		// adapter-auto는 Vercel을 감지해서 알아서 최적의 설정을 해줍니다.
+		adapter: adapter()
 	}
 };
 
